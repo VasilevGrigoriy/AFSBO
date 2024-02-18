a9d133731697d03fb6e1fb133c340d1109fd7e36
VasilevGrigoriy <vasiliev-greg@mail.ru>
2024-02-18 22:24:56 +0300

    refactor: add seq data adding logic to cleaning

--- afsbo/clean_data/clean_data.py
+++ afsbo/clean_data/clean_data.py
@@ -1,8 +1,9 @@
 import datetime
 import logging
-import sys
+import os
 from pathlib import Path
 
+import click
 import findspark
 import pyspark
 from pyspark.sql import SparkSession
@@ -10,56 +11,20 @@ from pyspark.sql import functions as f
 from pyspark.sql.types import DoubleType, IntegerType, ShortType, TimestampType
 from tqdm import tqdm
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setFormatter(
-    logging.Formatter(fmt="[%(asctime)s: %(levelname)s %(name)s] %(message)s")
-)
-logger.addHandler(handler)
+from afsbo.utils import init_basic_logger
 
-FILENAMES = [
-    "2019-08-22.txt",
-    "2019-09-21.txt",
-    "2019-10-21.txt",
-    "2019-11-20.txt",
-    "2019-12-20.txt",
-    "2020-01-19.txt",
-    "2020-02-18.txt",
-    "2020-03-19.txt",
-    "2020-04-18.txt",
-    "2020-05-18.txt",
-    "2020-06-17.txt",
-    "2020-07-17.txt",
-    "2020-08-16.txt",
-    "2020-09-15.txt",
-    "2020-10-15.txt",
-    "2020-11-14.txt",
-    "2020-12-14.txt",
-    "2021-01-13.txt",
-    "2021-02-12.txt",
-    "2021-03-14.txt",
-    "2021-04-13.txt",
-    "2021-05-13.txt",
-    "2021-06-12.txt",
-    "2021-07-12.txt",
-    "2021-08-11.txt",
-    "2021-09-10.txt",
-    "2021-10-10.txt",
-    "2021-11-09.txt",
-    "2021-12-09.txt",
-    "2022-01-08.txt",
-    "2022-02-07.txt",
-    "2022-03-09.txt",
-    "2022-04-08.txt",
-    "2022-05-08.txt",
-    "2022-06-07.txt",
-    "2022-07-07.txt",
-    "2022-08-06.txt",
-    "2022-09-05.txt",
-    "2022-10-05.txt",
-    "2022-11-04.txt",
-]
+logger = init_basic_logger(__name__, logging.DEBUG)
+
+# Эмулируем последовательно поступающие данные
+if "DATE" not in os.environ:
+    os.environ["DATE"] = "2019-08-22"
+CURRENT_DATE = datetime.date.fromisoformat(os.environ.get("DATE"))
+logger.debug("Current date in system env - %s", str(CURRENT_DATE))
+# Обновляем дату на следующий месяц
+os.environ["DATE"] = str(CURRENT_DATE + datetime.timedelta(days=31))
+logger.debug(
+    "Update system date by 31 days. Next run date will be %s", os.environ.get("DATE")
+)
 
 findspark.init()
 findspark.find()
@@ -130,42 +95,86 @@ def clean_data(spark_dataframe: pyspark.sql.DataFrame) -> pyspark.sql.DataFrame:
 
 def process_file(
     filename: str,
-    s3_raw_files_folder: str,
-    s3_processed_files_folder: str,
+    s3_raw_files_folder: Path,
+    s3_processed_files_folder: Path,
     spark: SparkSession,
-    time_done: str,
 ) -> None:
-    filepath = s3_raw_files_folder + filename
+    filepath = str(s3_raw_files_folder) + filename
     try:
         logger.debug("Reading data file...")
         data = load_spark_dataset(spark, filepath)
         logger.debug("Cleaning data file...")
         data = clean_data(data)
 
-        save_path = s3_processed_files_folder + f"{time_done}_{filename[:-4]}"
+        save_path = str(s3_processed_files_folder) + filename[:-4]
         data.write.parquet(save_path)
         logger.debug(f"Cleaned data saved in {save_path}")
     except Exception as e:
         logger.debug(f"Problems with file {filename}. Error: {str(e)}")
 
 
-def main():
-    logger.debug("Initializing spark session...")
-    time_done = str(datetime.datetime.now())
+def get_raw_filenames_by_date(
+    files_folder: Path, current_date: datetime.date
+) -> list[str]:
+    dates_update = [
+        datetime.date.fromisoformat(str(filename.name)[:-4])
+        for filename in files_folder.rglob("*.txt")
+    ]
+    raw_files = [str(date_) + ".txt" for date_ in dates_update if date_ <= current_date]
+    logger.debug(
+        "Found %s raw files in %s by date %s",
+        len(raw_files),
+        str(files_folder),
+        str(current_date),
+    )
+    return raw_files
+
+
+def get_processed_filenames(files_folder: Path) -> list[str]:
+    return [str(filename.name) for filename in files_folder.glob("*.parquet")]
+
+
+@click.command()
+@click.option(
+    "--s3_raw_files_folder",
+    default="s3a://mlops-otus-task2/raw_data/",
+    type=Path,
+    help="Path to folder with raw data in s3",
+)
+@click.option(
+    "--s3_processed_files_folder",
+    default="s3a://mlops-otus-task2/processed_data/",
+    type=Path,
+    help="Path to folder with already processed data in s3",
+)
+def main(s3_raw_files_folder: Path, s3_processed_files_folder: Path):
+    logger.debug("Initializing spark session")
     spark = (
         SparkSession.builder.appName("OTUS")
         .config("spark.dynamicAllocation.enabled", "true")
-        .config("spark.executor.memory", "10g")
-        .config("spark.driver.memory", "10g")
+        .config("spark.executor.memory", "30g")
+        .config("spark.driver.memory", "30g")
         .getOrCreate()
     )
-    for filename in tqdm(FILENAMES, desc="Processing files", total=len(FILENAMES)):
+    raw_filenames = get_raw_filenames_by_date(s3_raw_files_folder, CURRENT_DATE)
+    processed_filenames = get_processed_filenames(s3_processed_files_folder)
+    filenames_to_process = [
+        filename for filename in raw_filenames if filename not in processed_filenames
+    ]
+    logger.debug(
+        "Found %s raw files, %s already processed files. Need to process - %s files",
+        len(raw_filenames),
+        len(processed_filenames),
+        len(filenames_to_process),
+    )
+    for filename in tqdm(
+        filenames_to_process, desc="Processing files", total=len(filenames_to_process)
+    ):
         process_file(
             filename,
-            "s3a://mlops-otus-task2/raw_data/",
-            "s3a://mlops-otus-task2/processed_data/",
+            s3_raw_files_folder,
+            s3_processed_files_folder,
             spark,
-            time_done,
         )
 
 
