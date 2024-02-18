18695413f6941e31ff6ee3fa11ca96c09d57389a
VasilevGrigoriy <vasiliev-greg@mail.ru>
2024-02-18 22:49:19 +0300

    refactor: better code

--- dags/clean_data.py
+++ dags/clean_data.py
@@ -5,7 +5,7 @@ from airflow.providers.ssh.hooks.ssh import SSHHook
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.utils.dates import days_ago
 
-sshHook = SSHHook(ssh_conn_id="better_spark", cmd_timeout=18000, conn_timeout=18000)
+sshHook = SSHHook(ssh_conn_id="spark_server", cmd_timeout=18000, conn_timeout=18000)
 
 default_args = {
     "owner": "airflow",
@@ -21,15 +21,25 @@ with DAG(
     dag_id="clean-data-with-spark-cluster",
     default_args=default_args,
     start_date=days_ago(0),
-    schedule_interval="0 */5 * * *",
-    dagrun_timeout=timedelta(hours=5),
+    schedule_interval="0 0 * * *",
+    dagrun_timeout=timedelta(hours=3),
     description="Clear data from s3 using spark script",
 ) as dag:
 
-    run_this = SSHOperator(
+    clean_data = SSHOperator(
         task_id="clear-data",
-        command="cd project/ && pip install -r requirements.txt && python3 afsbo/clear_data/clear_data.py",
+        command="cd project/\
+            && curl -sSL https://install.python-poetry.org | python3\
+            && export PATH='/root/.local/bin:$PATH'\
+            && poetry shell\
+            && poetry install --no-interaction --no-ansi -vvv --with=dev\
+            && poetry install --no-interaction --no-ansi -vvv --with=pyspark\
+            && poetry install --no-interaction --no-ansi -vvv --with=s3\
+            && poetry install --no-interaction --no-ansi -vvv --with=logs\
+            && poetry install --no-interaction --no-ansi -vvv --with=click\
+            && poetry install --no-interaction --no-ansi -vvv --with=airflow\
+            && python3 afsbo/clear_data/clear_data.py",
         ssh_hook=sshHook,
     )
 
-run_this
+clean_data
